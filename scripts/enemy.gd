fd3bb2bbf23d51694af36d1ff47174bd8cd2eda4
DisasterUnknown <priyashanthafdo26@gmail.com>
2024-11-29 14:42:38 +0530

    added knockback in the slimes side and animations

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,73 +6,101 @@ var speed = 85
 var player_chase = false
 var player = null
 
-var health = 100
+var health = 60
 var player_inAtk_range = false
 var can_take_dmg = true
 var dying = false
-
-func audio_play(movement):
-	if movement == 1:
-		if (!slimeAudioWalk.playing):
-			slimeAudioWalk.play()
-	else:
-		slimeAudioWalk.stop()
+var knockback = false
 
 
+# Runs all the audio related to the slime ccording to the parramenters
+func audio_play(movement, track):
+	if track == "walk":
+		if movement == 1:
+			if (!slimeAudioWalk.playing):
+				slimeAudioWalk.play()
+		else:
+			slimeAudioWalk.stop()
+	if track == "death":
+		$AudioStreamSlime_death.play()
+
+
+# Runs every physics frame
 func _physics_process(delta):
 	deal_with_dmg()
 	
 	if !dying:
-		if player_chase:
+		if knockback:
+			var knockValue = (position - player.position).normalized() * (speed * 3)
+			var knockback_move = move_and_collide(knockValue * delta)
+			await get_tree().create_timer(0.2).timeout
+			knockback = false
+		elif player_chase:
 			position += (player.position - position).normalized() * speed * delta
 			move_and_collide(Vector2(0,0))
 
 			$AnimatedSprite2D.play("walk")
-			audio_play(1)
+			audio_play(1, "walk")
 		else:
 			$AnimatedSprite2D.play("idle")
-			audio_play(0)
+			audio_play(0, "walk")
 
 
+# Creates an enemy method
 func enemy():
 	pass
 
 
+# When a player enters the detection area
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
 
 
+# When a player leaves the detection area
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
 
 
+# When the player is in the atk range
 func _on_enemy_hitbox_body_entered(body):
 	if body.has_method("player"):
 		player_inAtk_range = true
 
 
+# When the player exits the atk range
 func _on_enemy_hitbox_body_exited(body):
 	if body.has_method("player"):
 		player_inAtk_range = false		
 
 
+# If the player is attacking
 func deal_with_dmg():
 	if player_inAtk_range and Global.player_current_atk and can_take_dmg:
 		health = health - 20
 		can_take_dmg = false
 		$gainDmg_cooldown.start()
+		dmgAnimation()
 		print("Slime Health: ", health)
 		if health <= 0:
 			dying = true
 			$AnimatedSprite2D.play("death")
-			$death_ani_cooldown.start()
+			audio_play(1, "death")
+			await get_tree().create_timer(0.6).timeout         # Waits for a few seconds
+			self.queue_free()
 
 
+# Get dmg color animation
+func dmgAnimation():
+	if health > 0:
+		await get_tree().create_timer(0.2).timeout
+		$AnimatedSprite2D.modulate = Color.RED
+		knockback = true
+		await get_tree().create_timer(0.2).timeout
+		$AnimatedSprite2D.modulate = Color.WHITE
+
+
+# Dmg gaining cooldown timer
 func _on_gain_dmg_cooldown_timeout():
 	can_take_dmg = true
-
-
-func _on_death_ani_cooldown_timeout():
-	self.queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -13,9 +13,11 @@ const speed = 150
 var attacking = false
 var current_dir = "none"
 
+# Runs when the code is ready to be sxercuted
 func _ready():
 	$AnimatedSprite2D.play("idle_front")
 
+# This function runs evey frome itteration (ones evey game cycle)
 func _physics_process(delta):
 	enemy_atk()
 	attack()
@@ -30,48 +32,54 @@ func _physics_process(delta):
 		self.queue_free()
 	
 	
+# Player movement related code
 func player_movement(delta):
 	if Input.is_action_pressed("Right_Move"):
 		current_dir = "right"
 		play_anim(1)
-		audio_play(1)
+		audio_play(1, "walk")
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("Left_Move"):
 		current_dir = "left"
 		play_anim(1)
-		audio_play(1)
+		audio_play(1, "walk")
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("Up_Move"):
 		current_dir = "up"
 		play_anim(1)
-		audio_play(1)
+		audio_play(1, "walk")
 		velocity.x = 0
 		velocity.y = -speed
 	elif Input.is_action_pressed("Down_Move"):
 		current_dir = "down"
 		play_anim(1)
-		audio_play(1)
+		audio_play(1, "walk")
 		velocity.x = 0
 		velocity.y = speed
 	else:
 		play_anim(0)
-		audio_play(0)
+		audio_play(0, "walk")
 		velocity.x = 0
 		velocity.y = 0
 		
 	move_and_slide() 
 	
 
-func audio_play(movement):
-	if movement == 1:
-		if (!playerAudioWalk.playing):
-			playerAudioWalk.play()
-	else:
-		playerAudioWalk.stop()
+# Runs all the audio related to the player ccording to the parramenters
+func audio_play(movement, track):
+	if track == "walk":
+		if movement == 1:
+			if (!playerAudioWalk.playing):
+				playerAudioWalk.play()
+		else:
+			playerAudioWalk.stop()
+	elif track == "atk":
+		$AudioStreamPlayer_attack.play()
 
 
+# Runs player animation related for walking
 func play_anim(movement):
 	var dir = current_dir
 	var anim = $AnimatedSprite2D
@@ -106,33 +114,38 @@ func play_anim(movement):
 				anim.play("idle_front")
 	
 	
-
+# Creates a player method
 func player():
 	pass
 
 
+# what todo when the enemy enters the player body
 func _on_player_hitbox_body_entered(body):
 	if body.has_method("enemy"):
 		enemy_inAtk_range = true
 
 
+# what todo when the enemy exits the player body
 func _on_player_hitbox_body_exited(body):
 	if body.has_method("enemy"):
 		enemy_inAtk_range = false
 
 
+# When enemy is in attack range
 func enemy_atk():
 	if enemy_inAtk_range and atk_cooldown:
-		health = health - 20
+		#health = health - 20
 		atk_cooldown = false
 		$gainDmg_cooldown.start()
 		print("Player Health: ", health)
 
 
+# Attack cooldown timer
 func _on_atk_cooldown_timeout():
 	atk_cooldown = true
 
 
+# Player attack run animation and audio
 func attack():
 	var dir = current_dir
 	
@@ -145,18 +158,23 @@ func attack():
 			$AnimatedSprite2D.flip_h = false
 			$AnimatedSprite2D.play("side_attack")
 			$dealAtk_timer.start()
+			audio_play(0, "atk")
 		if dir == "left":
 			$AnimatedSprite2D.flip_h = true
 			$AnimatedSprite2D.play("side_attack")
 			$dealAtk_timer.start()
+			audio_play(0, "atk")
 		if dir == "up":
 			$AnimatedSprite2D.play("back_attack")
 			$dealAtk_timer.start()
+			audio_play(0, "atk")
 		if dir == "down":
 			$AnimatedSprite2D.play("front_attack")
 			$dealAtk_timer.start()
+			audio_play(0, "atk")
 
 
+# When the attack timer goes off
 func _on_deal_atk_timer_timeout():
 	$dealAtk_timer.stop()
 	attacking = false
